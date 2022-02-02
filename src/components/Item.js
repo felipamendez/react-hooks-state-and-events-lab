64b82a2b832e10148a7b6ba8c354dc7ca2fcfe3c
Felipa Mendez <felipalouise@gmail.com>
2022-02-02 16:43:35 -0600

    commit tree 67ce5178042b1796ebf6713d94812044623aa57f

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false)
+  
+  const itemClass = inCart ? "in-cart" : " "
+  const buttonText = inCart ? "Remove From Cart" : "Add to Cart"
+  const buttonClass = inCart ? "remove" : "add"  
+
+  function clickHandler () {
+    setInCart((inCart) => !inCart)
+  }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={clickHandler}>{buttonText}</button>
     </li>
   );
 }
